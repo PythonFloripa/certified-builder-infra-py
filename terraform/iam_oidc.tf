28dfaa864d0541bc991a1eea0748dae25c4d3908
bento <bentoluizv@gmail.com>
2025-11-13 20:02:47 -0300

    Remove GitHub Actions OIDC role conditions for dev branch

--- terraform/iam_oidc.tf
+++ terraform/iam_oidc.tf
@@ -19,10 +19,7 @@ resource "aws_iam_role" "github_actions" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = [
-              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main",
-              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/dev"
-            ]
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"
           }
         }
       }
