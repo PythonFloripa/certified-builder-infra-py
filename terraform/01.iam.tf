00a4debc948a24a1e7cd5e5c569d08d359912902
bento <bentoluizv@gmail.com>
2025-11-14 14:23:15 -0300

    commit super semmantico

--- terraform/01.iam.tf
+++ terraform/01.iam.tf
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "github_action_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_org}/*"]
+      values   = ["repo:${var.github_org}/${var.github_repo}:*"]
     }
   }
 }
@@ -36,109 +36,114 @@ resource "aws_iam_role" "github_actions_assume_role" {
   assume_role_policy = data.aws_iam_policy_document.github_action_assume_role.json
 }
 
-# ### IAM Permissions for Github Action Role
-# data "aws_iam_policy_document" "gh_policy_document" {
-#   # SQS
-#   statement {
-#     effect = "Allow"
+### IAM Permissions for Github Action Role
+data "aws_iam_policy_document" "github_action_permissions" {
+  #   # SQS
+  #   statement {
+  #     effect = "Allow"
 
-#     actions = [
+  #     actions = [
 
-#     ]
+  #     ]
 
-#     resources = [
-#       "arn:aws:sqs:${var.aws_region}:${var.aws_account_id}:${var.project_name}-*",
-#     ]
-#   }
-#   # ECR
-#   statement {
-#     effect = "Allow"
+  #     resources = [
+  #       "arn:aws:sqs:${var.aws_region}:${var.aws_account_id}:${var.project_name}-*",
+  #     ]
+  #   }
+  #   # ECR
+  #   statement {
+  #     effect = "Allow"
 
-#     actions = [
+  #     actions = [
 
-#     ]
+  #     ]
 
-#     resources = [
-#       "arn:aws:ecr:${var.aws_region}:${var.aws_account_id}:repository/${var.project_name}-*",
-#     ]
-#   }
-#   # DynamoDB
-#   statement {
-#     effect = "Allow"
+  #     resources = [
+  #       "arn:aws:ecr:${var.aws_region}:${var.aws_account_id}:repository/${var.project_name}-*",
+  #     ]
+  #   }
+  #   # DynamoDB
+  #   statement {
+  #     effect = "Allow"
 
-#     actions = [
+  #     actions = [
 
-#     ]
-#     resources = [
-#       "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/${var.project_name}-*",
-#     ]
-#   }
-#   # API Gateway
-#   statement {
-#     effect = "Allow"
+  #     ]
+  #     resources = [
+  #       "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/${var.project_name}-*",
+  #     ]
+  #   }
+  #   # API Gateway
+  #   statement {
+  #     effect = "Allow"
 
-#     actions = [
-#     ]
+  #     actions = [
+  #     ]
 
-#     resources = [
-#       "arn:aws:apigateway:*::/*",
-#     ]
-#   }
-#   # Lambda
-#   statement {
-#     effect = "Allow"
+  #     resources = [
+  #       "arn:aws:apigateway:*::/*",
+  #     ]
+  #   }
+  #   # Lambda
+  #   statement {
+  #     effect = "Allow"
 
-#     actions = [
+  #     actions = [
 
-#     ]
-#     resources = [
-#       "arn:aws:lambda:${var.aws_region}:${var.aws_account_id}:function:${var.project_name}-*",
-#     ]
-#   }
-#   # CloudWatch Logs
-#   statement {
-#     effect = "Allow"
+  #     ]
+  #     resources = [
+  #       "arn:aws:lambda:${var.aws_region}:${var.aws_account_id}:function:${var.project_name}-*",
+  #     ]
+  #   }
+  #   # CloudWatch Logs
+  #   statement {
+  #     effect = "Allow"
 
-#     actions = [
+  #     actions = [
 
-#     ]
+  #     ]
 
-#     resources = [
-#       "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:${var.project_name}-*",
-#     ]
-#   }
+  #     resources = [
+  #       "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:${var.project_name}-*",
+  #     ]
+  #   }
 
-#   # S3
-#   statement {
-#     effect = "Allow"
+  # S3
+  statement {
+    effect = "Allow"
 
-#     actions = [
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket",
+      "s3:DeleteObject",
+    ]
+    resources = [
+      "arn:aws:s3:::tech-floripa-certificates-dev-tf-state",
+      "arn:aws:s3:::tech-floripa-certificates-dev-tf-state/*"
+    ]
+  }
 
-#     ]
+  #   # IAM
+  #   statement {
+  #     effect = "Allow"
 
-#     resources = ["*"]
-#   }
+  #     actions = [
+  #     ]
 
-#   # IAM
-#   statement {
-#     effect = "Allow"
+  #     resources = [
 
-#     actions = [
-#     ]
+  #     ]
+  #   }
+}
 
-#     resources = [
+resource "aws_iam_policy" "github_actions_policy" {
+  name        = "github-actions-policy"
+  description = "Permissions for GitHub Actions to manage project resources"
+  policy      = data.aws_iam_policy_document.github_action_permissions.json
+}
 
-#     ]
-#   }
-# }
-
-# resource "aws_iam_policy" "github_actions_policy" {
-#   name        = "github-actions-policy"
-#   description = "Permissions for GitHub Actions to manage project resources"
-#   policy      = data.       aws_iam_policy_document.github_actions_policy.json
-# }
-
-# resource "aws_iam_role_policy   _attachment" "github_actions_policy_attachment" {
-#   role       = aws_iam_role.github_actions_assume_role.name
-#   policy_arn = aws_iam_policy.github_actions_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "github_actions_policy_attachment" {
+  role       = aws_iam_role.github_actions_assume_role.name
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+}
