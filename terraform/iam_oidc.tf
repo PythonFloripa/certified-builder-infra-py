557094a87de1d604a7e416625e02b2599da70e90
bento <bentoluizv@gmail.com>
2025-11-13 21:51:58 -0300

    Update IAM role condition to allow access for main and feature branches in GitHub Actions

--- terraform/iam_oidc.tf
+++ terraform/iam_oidc.tf
@@ -18,8 +18,13 @@ resource "aws_iam_role" "github_actions" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/*"
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main",
+              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/feat/*"
+            ]
           }
         }
       }
