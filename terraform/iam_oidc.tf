0742c3dc7f495e2651c28aa39b501b0bd61a99a0
bento <bentoluizv@gmail.com>
2025-11-13 21:45:30 -0300

    Update OIDC role condition to allow all branches for GitHub Actions

--- terraform/iam_oidc.tf
+++ terraform/iam_oidc.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "github_actions" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/*"
           }
         }
       }
