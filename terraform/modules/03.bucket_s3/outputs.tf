c0e315b7a020f15cb29819594002bee594d43201
bento <bentoluizv@gmail.com>
2025-11-30 18:23:24 -0300

    Rename S3 bucket resources for clarity and consistency

--- terraform/modules/03.bucket_s3/outputs.tf
+++ terraform/modules/03.bucket_s3/outputs.tf
@@ -2,15 +2,15 @@
 
 output "bucket_name" {
   description = "Nome do bucket S3"
-  value       = aws_s3_bucket.bucket.id
+  value       = aws_s3_bucket.certificates_bucket.id
 }
 
 output "bucket_arn" {
   description = "ARN do bucket S3"
-  value       = aws_s3_bucket.bucket.arn
+  value       = aws_s3_bucket.certificates_bucket.arn
 }
 
 output "lifecycle_rule" {
   description = "Regras de ciclo de vida dos objetos S3"
-  value       = length(aws_s3_bucket_lifecycle_configuration.bucket_lifecycle) > 0 ? aws_s3_bucket_lifecycle_configuration.bucket_lifecycle[0] : null
+  value       = length(aws_s3_bucket_lifecycle_configuration.certificates_bucket_lifecycle_config) > 0 ? aws_s3_bucket_lifecycle_configuration.bucket_lifecycle[0] : null
 }
